4dc45d4b9304960e293c001416ecbeb7e3584df8
dtrihinas <trihinas@cs.ucy.ac.cy>
2013-09-28 14:53:47 +0300

    Convert to maven project #2

---
+++ JCatascopia-Agent/src/main/java/mypack/myclass.java
@@ -0,0 +1,15 @@
+package mypack;
+
+import org.jeromq.ZMQ;
+
+public class myclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(ZMQ.getVersionString());
+
+	}
+
+}
