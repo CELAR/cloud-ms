0c36c1981b2a0f8a1b6e0c99be7e6cb1adbbefb4
Nicholas Loulloudes <loulloudes.n@cs.ucy.ac.cy>
2013-10-16 16:49:08 +0300

    Improved JCatascopia-Agent-start.sh to get the final name of the JAR from the MAVEN project. Update the assembly/bin.xml to reflect the above change.

--- JCatascopia-Agent/target/classes/JCatascopia-Agent-start.sh
+++ JCatascopia-Agent/target/classes/JCatascopia-Agent-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+JAR="JCatascopia-Agent-0.0.1-SNAPSHOT.jar"
 JCATASCOPIA_AGENT_HOME="/usr/local/bin/JCatascopiaAgentDir"
-java -jar $JCATASCOPIA_AGENT_HOME/JCatascopia-Agent.jar  $JCATASCOPIA_AGENT_HOME & 
+java -jar $JCATASCOPIA_AGENT_HOME/$JAR  $JCATASCOPIA_AGENT_HOME & 
