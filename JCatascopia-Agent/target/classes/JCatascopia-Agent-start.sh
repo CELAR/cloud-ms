48babddc6bcc9ecb907003a8a456d4407104bf5d
Nicholas Loulloudes <loulloudes.n@cs.ucy.ac.cy>
2013-10-24 10:39:10 +0300

    Removing the target directory

--- JCatascopia-Agent/target/classes/JCatascopia-Agent-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-JAR="JCatascopia-Agent-0.0.1-SNAPSHOT.jar"
-JCATASCOPIA_AGENT_HOME="/usr/local/bin/JCatascopiaAgentDir"
-java -jar $JCATASCOPIA_AGENT_HOME/$JAR  $JCATASCOPIA_AGENT_HOME & 
